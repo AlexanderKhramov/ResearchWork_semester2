3886c260001efbc3ad889dc3eee947a37ba120c2
Alexander Khramov <alexsandr.khramov@gmail.com>
2023-05-24 23:00:49 +0300

    finalized read_column function added read_column_date function (for reading year and month information) added statistic_by_column function, main function for analysing added data anlysis and comments in main function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <windows.h>
-#include <time.h>
+//#include <time.h>
 #include <cstdlib>
 #include <fstream>
 #include <vector>
@@ -11,6 +11,8 @@
 #define DATA_FILENAME "data.csv"
 
 using namespace std;
+
+enum {STATION,NAME,LATITUDE,LONGITUDE,ELEVATION,DATE_,CDSD,DSND,EMNT,EMSD,EMXP,EMXT,HDSD,PRCP,TAVG,TMAX,TMIN};
 
 
 void check(){
@@ -49,43 +51,18 @@ int read_column(int col_no, double* x, double* y)
 {
 	fstream fin;
 	fin.open(DATA_FILENAME, ios::in);
-	int roll2, count = 0, date_no, n = 0;
+	int n = 0;
 	vector<string> row;
 	string line, word, temp;
 	row.clear();
     getline(fin, line, '\n');
-//    stringstream s;
-//    s << line;
-//    cout << line << endl;
-//    getline(fin, line, '\n');
-//    cout << line << endl;
-//    getline(fin, line, '\n');
-//    cout << line << endl;
-//    while (getline(s, word, ';')) {
-//        row.push_back(word);
-//        count++;
-//    }
-//    cout << col_no << endl;
-//    cout << row[col_no] << endl;
 	while (getline(fin, line, '\n')) {
         stringstream ss;
 		ss << line;
-		cout << line << endl;
-//		if (n == 0){
-//            cout << "WORDS:" << endl;
-//		}
         row.clear();
 		while (getline(ss, word, ';')) {
-//            cout << word << " ";
 			row.push_back(word);
 		}
-//		if (n == 0)
-//            cout << "ROWS:" << endl;
-//		if (n == 0){
-//            for (int i = 0; i < 17; i++)
-//                cout << row[i] << endl;
-//		}
-//		if (n != 0)
         if (!row[col_no].empty()){
             x[n] = n;
             y[n] = stof(row[col_no]);
@@ -95,16 +72,157 @@ int read_column(int col_no, double* x, double* y)
 	return n;
 }
 
+int read_column_date(int col_no, double* year, double* month, double* y){
+	ifstream fin;
+	fin.open(DATA_FILENAME, ios::in);
+	int n = 0;
+	vector<string> row;
+	string line, word, temp;
+	row.clear();
+    getline(fin, line, '\n');
+	while (getline(fin, line, '\n')) {
+        stringstream ss;
+		ss << line << ";";
+//		cout << line;
+        row.clear();
+		while (getline(ss, word, ';')) {
+			row.push_back(word);
+		}
+        if (!row[col_no].empty() ){
+            stringstream ss;
+            ss << row[DATE_];
+            getline(ss, word, '-');
+            year[n] = stof(word);
+            getline(ss, word, '-');
+            month[n] = stof(word);
+            y[n] = stof(row[col_no]);
+            ++n;
+        }
+	}
+	fin.close();
+	return n;
+}
 
+typedef int (*month_cond) (double m);
+
+void statistics_by_column(int col, month_cond cond){
+    int n, i1, i2;
+    double *y = new double[N], a, b, *year = new double[N], *month = new double[N];
+    double min_y = 1000000, max_y = -1;
+    if (y == NULL || year == NULL || month == NULL)
+        cout << "0_o";
+    n = read_column_date(col, year, month, y);
+    for (i1 = 0, i2 = 0; i2 < n; i2++){
+        if (cond(month[i2])){
+            year[i1] = year[i2];
+            month[i1] = month[i2];
+            y[i1] = y[i2];
+            if (y[i1] > max_y)
+                max_y = y[i1];
+            if (y[i1] < min_y)
+                min_y = y[i1];
+            ++i1;
+        }
+    }
+    n = i1;
+    head(year, 7);
+    head(month, 7);
+    head(y, 7);
+    cout << "min: " << min_y << " max: " << max_y << endl;
+    cout << "n: " << n << endl;
+    if (n >= 0){
+        coefficients(year, y, n, &a, &b, 0);
+        cout << "a: " << a << endl << "b: " << b << endl;
+    } else {
+        cout << "NO SUCH DATA" << endl;
+    }
+    delete y;
+    delete year;
+    delete month;
+}
+
+int summer_month(double m){
+    return m <= 8 && m >= 6;
+}
+
+int winter_month(double m){
+    return m <= 2 || m == 12;
+}
+
+int january_month(double m){
+    return m == 1;
+}
 
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    srand(time(0));
-    double *x = new double[N], *y = new double[N];
-    read_column(12, x, y);
-    head(x, 7);
-    head(y, 7);
+
+    cout << "–ассмотрим температурные характеристики дл€ зимних мес€цев";
+
+    cout << endl << "ћинимальна€ температура за мес€ц (EMNT)" << endl;
+    statistics_by_column(EMNT, winter_month);
+
+    cout << endl << "ћаксимальна€ температура за мес€ц (EMXT)" << endl;
+    statistics_by_column(EMXT, winter_month);
+
+    cout << endl << "—редн€€ минимальна€ температура за мес€ц (TMIN)" << endl;
+    statistics_by_column(TMIN, winter_month);
+
+    cout << endl << "—редн€€ максимальна€ температура за мес€ц (TMAX)" << endl;
+    statistics_by_column(TMAX, winter_month);
+
+    cout << endl << "√радусо-дни охлаждени€ (CDSD)" << endl;
+    statistics_by_column(CDSD, winter_month);
+
+    cout << endl << "√радусо-дни нагрева (HDSD)" << endl;
+    statistics_by_column(HDSD, winter_month);
+
+
+    cout << "» то же самое в летние мес€цы";
+
+    cout << endl << "Ќаименьша€ температура за мес€ц (EMNT)" << endl;
+    statistics_by_column(EMNT, summer_month);
+
+    cout << endl << "ћаксимальна€ температура за мес€ц (EMXT)" << endl;
+    statistics_by_column(EMXT, winter_month);
+
+    cout << endl << "—редн€€ минимальна€ температура за мес€ц (TMIN)" << endl;
+    statistics_by_column(TMIN, summer_month);
+
+    cout << endl << "—редн€€ максимальна€ температура за мес€ц (TMAX)" << endl;
+    statistics_by_column(TMAX, summer_month);
+
+    cout << endl << "√радусо-дни охлаждени€ (CDSD)" << endl;
+    statistics_by_column(CDSD, summer_month);
+
+    cout << endl << "√радусо-дни нагрева (HDSD)" << endl;
+    statistics_by_column(HDSD, summer_month);
+
+
+    cout << "»тог: общий тренд всех температурных характеристик - рост. ќсобенно он заметен зимой в отношении характеристик EMNT и TMIN, которые отвечают за минимальную температуру." << endl;
+    cout << "≈щЄ сильнее мен€ютс€ показатели HDSD и CDSD (градусо-дни нагрева и охлаждени€). ѕричЄм HDSD и летом, и зимой падает, а CDSD растЄт, что соотноситс€ с тем, что температура растЄт (если на улице теплее, отапливать помещени€ нужно меньше)." << endl;
+
+//
+    cout << endl << "ѕроанализируем количество снега и осадков" << endl;
+    cout << " оличество снежных дней (в зимние мес€цы) (DSND)" << endl;
+    statistics_by_column(DSND, winter_month);
+
+    cout << endl << "ћаксимальный уровень снега (в зимние мес€цы) (EMSD)" << endl;
+    statistics_by_column(EMSD, winter_month);
+    cout << " оличество снежных дней увеличиваетс€ при том, что максимальный уровень снега падает" << endl;
+
+    cout << endl << "ќбщее количество осадков (в зимние мес€цы) (PRCP)" << endl;
+    statistics_by_column(PRCP, winter_month);
+
+    cout << endl << "ћаксимальное количество осадков (в зимние мес€цы) (EMXP)" << endl;
+    statistics_by_column(EMXP, winter_month);
+
+    cout << endl << "ќбщее количество осадков (в летние мес€цы) (PRCP)" << endl;
+    statistics_by_column(PRCP, summer_month);
+
+    cout << endl << "ћаксимальное количество осадков (в летние мес€цы) (EMXP)" << endl;
+    statistics_by_column(EMXP, summer_month);
+    cout << " оличество осадков с каждым годом увеличиваетс€, хот€ не так сильно как, например, температура." << endl;
     return 0;
 }
